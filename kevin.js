dabcf3024184976cd0f040aa2593c450089ec301
dancingshell <kaitlinjaffe@gmail.com>
2014-11-04 19:14:55 -0800

    modified nav styling and made navbar responsive

--- kevin.js
+++ kevin.js
@@ -2,20 +2,25 @@ function hover(link) {
   if (link == "furniture") 
   {
     document.getElementById("header").style.backgroundColor="rgba(1, 155, 155, .4)";
-    
   }
   else if (link == "art") 
   {
     document.getElementById("header").style.backgroundColor="rgba(120, 155, 35, .4)";
   }
-  else if (link == "about") 
+  else if (link == "home") 
+  {
+    document.getElementById("header").style.backgroundColor="rgba(125, 225, 205, .4)";
+  }
+  else if (link == "contact") 
   {
     document.getElementById("header").style.backgroundColor="rgba(190, 160, 25, .4)";
   }
-  document.getElementById(link).style.borderBottom="1px solid white";
+  // document.getElementById(link).style.borderBottom="1px solid white";
+  document.getElementById(link).style.color="white";
 }
 
 
 function unhover(link) {
-  document.getElementById(link).style.borderBottom="0";
+  // document.getElementById(link).style.borderBottom="0";
+  document.getElementById(link).style.color="black";
 }
